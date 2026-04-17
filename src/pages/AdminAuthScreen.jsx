37fb0b4bb8ea7e09c459df440d097c4ead140309
Priyansh Sahu <priyansh@Priyanshs-MacBook-Air.local>
2026-04-17 17:37:03 +0530

    added dev url for login api

--- src/pages/AdminAuthScreen.jsx
+++ src/pages/AdminAuthScreen.jsx
@@ -23,7 +23,7 @@ const AdminAuthScreen = () => {
       setIsLoading(true);
       
       try {
-          const response = await fetch('http://localhost:5001/api/auth/login', {
+          const response = await fetch('https://pickcric-admin-backend.onrender.com/api/auth/login', {
               method: 'POST',
               headers: { 'Content-Type': 'application/json' },
               body: JSON.stringify({ email, password })
